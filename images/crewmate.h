cf17e4c509935c35a2a99b7e282a2917f77d9dcc
mhersi123 <91560918+mhersi123@users.noreply.github.com>
2021-10-12 14:32:07 -0400

    Create crewmate.h

---
+++ images/crewmate.h
@@ -0,0 +1,23 @@
+/*
+ * Exported with nin10kit v1.8
+ * Invocation command was nin10kit --mode=3 --resize=36x45 crewmate crewmate.png 
+ * Time-stamp: Thursday 04/08/2021, 18:46:01
+ * 
+ * Image Information
+ * -----------------
+ * crewmate.png 36@45
+ * 
+ * All bug reports / feature requests are to be filed here https://github.com/TricksterGuy/nin10kit/issues
+ */
+
+#ifndef CREWMATE_H
+#define CREWMATE_H
+
+extern const unsigned short crewmate[1620];
+#define CREWMATE_SIZE 3240
+#define CREWMATE_LENGTH 1620
+#define CREWMATE_WIDTH 36
+#define CREWMATE_HEIGHT 45
+
+#endif
+
